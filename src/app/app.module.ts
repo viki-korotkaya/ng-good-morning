90535d2881599975442e4475ce10db4a70e2695b
viki-korotkaya <viki.korotkaya@gmail.com>
2021-01-27 11:05:46 -0500

    Added main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ShopBuilderComponent } from './shop-builder/shop-builder.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ShopBuilderComponent,
+    OrdersComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shop-builder/shop-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-builder',
+  templateUrl: './shop-builder.component.html',
+  styleUrls: ['./shop-builder.component.css']
+})
+export class ShopBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
