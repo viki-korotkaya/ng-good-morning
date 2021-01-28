60f676a3794eddf7e7963d483e651ae674ec4251
viki-korotkaya <viki.korotkaya@gmail.com>
2021-01-27 20:35:43 -0500

    Added initial layout

--- src/app/shop-builder/build-control/build-control.component.ts
+++ src/app/shop-builder/build-control/build-control.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./build-control.component.css']
 })
 export class BuildControlComponent implements OnInit {
-
+  controls: { label: string, type: string, cost: string }[] = [
+    { label: 'Espresso', type: 'espresso', cost: '2.5$' },
+    { label: 'Americano', type: 'americano', cost: '2.0$' },
+    { label: 'Cappuccino', type: 'cappuccino', cost: '3.5$' },
+    { label: 'Latte', type: 'latte', cost: '3.2$' },
+    { label: 'Big Joe', type: 'bigjoe', cost: '5.0$' },
+    { label: 'Hot chocolate', type: 'hotchoc', cost: '4.5$' },
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAdd(){
+
+  }
+
+  onRemove(){
+
+  }
 }
