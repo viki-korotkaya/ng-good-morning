13378522c2ed8ed068e183bd1771fb38967702af
viki-korotkaya <viki.korotkaya@gmail.com>
2021-01-28 22:03:35 -0500

    Created methods of adding and removing items in build-control.component

--- src/app/shop-builder/build-control/build-control.component.ts
+++ src/app/shop-builder/build-control/build-control.component.ts
@@ -6,24 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./build-control.component.css']
 })
 export class BuildControlComponent implements OnInit {
-  controls: { label: string, type: string, cost: string }[] = [
-    { label: 'Espresso', type: 'espresso', cost: '2.5$' },
-    { label: 'Americano', type: 'americano', cost: '2.0$' },
-    { label: 'Cappuccino', type: 'cappuccino', cost: '3.5$' },
-    { label: 'Latte', type: 'latte', cost: '3.2$' },
-    { label: 'Big Joe', type: 'bigjoe', cost: '5.0$' },
-    { label: 'Hot chocolate', type: 'hotchoc', cost: '4.5$' },
+  controls: { label: string, type: string, cost: number }[] = [
+    { label: 'Espresso', type: 'espresso', cost: 2.5 },
+    { label: 'Americano', type: 'americano', cost: 2.0 },
+    { label: 'Cappuccino', type: 'cappuccino', cost: 3.5 },
+    { label: 'Latte', type: 'latte', cost: 3.2 },
+    { label: 'Big Joe', type: 'bigjoe', cost: 5.0 },
+    { label: 'Hot chocolate', type: 'hotchoc', cost: 4.5 },
   ];
+  purchase: {}[] = [];
+  price: number = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAdd(){
-
+  onAdd(type: string){
+    this.purchase[type] ? this.purchase[type] += 1 : this.purchase[type] = 1;
+    let currentControl = this.controls.filter(el => el.type === type);
+    this.price = + (this.price + currentControl[0].cost).toFixed(2);
   }
 
-  onRemove(){
-
+  onRemove(type: string){
+    if (this.purchase[type] > 0){
+      this.purchase[type] -= 1;
+      let currentControl = this.controls.filter(el => el.type === type);
+      this.price = + (this.price - currentControl[0].cost).toFixed(2);
+    }
   }
 }
